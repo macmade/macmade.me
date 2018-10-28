b67740b6e328f413a4790e4dd50823c9baf0c171
macmade <macmade@xs-labs.com>
2018-10-28 16:25:28 +0100

    Index page

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>macmade.me</title>
-</head>
-<body>
-    hello, world
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <!--
+        
+        ##################################################
+        #                                                #
+        #       Blood Sweat & Code (& Rock'N'Roll)       #
+        #      Thanx for looking at the source code      #
+        #                                                #
+        #                 XS-Labs © 2018                 #
+        #                                                #
+        ##################################################
+        
+        -->
+                
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Jean-David Gadina</title>
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="/css/base.css" type="text/css" media="all" />
+        <meta name="author" content="XS-Labs" />
+        <meta name="description" content="Jean-David Gadina - Software Engineer" />
+        <meta name="keywords" content="jean,david,gadina,macmade,software,engineer" />
+        <meta name="rating" content="General" />
+        <meta name="robots" content="all" />
+        <!--[if IE]><link rel="shortcut icon" href="/favicon.ico"><![endif]-->
+        <link rel="icon" href="/favicon.png">
+        <link rel="apple-touch-icon-precomposed" href="/favicon-apple-touch.png">
+    </head>
+    <body>
+        <div class="container">
+            <div class="text-center">
+                <h1>Jean-David Gadina</h1>
+                <h2>Software Engineer</h2>
+                <div>
+                    <img class="macmade" width="250" height="250" src="images/macmade.png" alt="Jean-David Gadina" title="Jean-David Gadina" />
+                </div>
+                <ul class="links">
+                    <li><a href="https://xs-labs.com">Website</a></li>
+                    <li><a href="https://xs-labs.com/en/blog/">Blog</a></li>
+                    <li><a href="https://github.com/macmade">GitHub</a></li>
+                    <li><a href="https://twitter.com/macmade">Twitter</a></li>
+                    <li><a href="https://www.linkedin.com/in/macmade/">LinkedIn</a></li>
+                    <li><a href="https://stackoverflow.com/users/182676/macmade">Stack Overflow</a></li>
+                </ul>
+            </div>
+        </div>
+        <script src="/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
 </html>
